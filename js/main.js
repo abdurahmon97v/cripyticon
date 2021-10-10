8840395085b8daa6ca339fd4f260ee196d16d98f
abdumovi <abdumovi@gmail.com>
2021-10-10 14:21:52 +0500

    end

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('.nav');
+let nav_list = document.querySelector('.nav__list');
+let btn = document.querySelector('.main-header__btn');
+let nav_btn = document.querySelector('.nav__link-btn');
+let span = document.querySelector('.main-header')
+btn.addEventListener('click',function(){
+  nav_btn.classList.toggle('btn');
+  span.classList.toggle('viz');
+  nav.classList.toggle('nav--height');
+  nav_list.classList.toggle('open');
+
+})
